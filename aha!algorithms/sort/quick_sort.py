d2f09be7ee92da4d89c9abf3574619e30b4d70a9
unknown <you@example.com>
2017-06-07 21:07:05 -0400

    fix quick_sort

--- aha!algorithms/sort/quick_sort.py
+++ aha!algorithms/sort/quick_sort.py
@@ -8,26 +8,32 @@ Created on 2016年1月18日
 from random_list import RandomList
 R = RandomList(5, 10)
 lst = R.gen()
-def sub_sort(array,low,high):
-    key = array[low]
-    while low < high:
-        while low < high and array[high] >= key:
-            high -= 1
-        while low < high and array[high] < key:
-            array[low] = array[high]
-            low += 1
-            array[high] = array[low]
-    array[low] = key
-    return low
 
-def quick_sort(array,low,high):
-    if low < high:
-        key_index = sub_sort(array,low,high)
-        quick_sort(array,low,key_index)
-        quick_sort(array,key_index+1,high)                   
-    
-    
-print(lst)        
-quick_sort(lst, 0, len(lst)-1)
+def path_sort(list, start_index,end_index):
+        flag = list[end_index]
+        i = start_index - 1
+        for j in range(start_index,end_index):
+            if list[j] > flag:
+                pass
+            else:
+                i += 1
+                tmp = list[i]
+                list[i] = list[j]
+                list[j] = tmp
+        tmp = list[end_index]
+        list[end_index] = list[i+1]
+        list[i+1] = tmp
+
+        return i+1
+
+def Quick_sort(list,start_index,end_index):
+        if start_index >= end_index:
+            return
+        middle = path_sort(list,start_index,end_index)
+        Quick_sort(list,start_index,middle-1)
+        Quick_sort(list,middle+1,end_index)
+        print(list)
+
+Quick_sort(lst,0,len(lst)-1)                       
 print(lst)       
 
